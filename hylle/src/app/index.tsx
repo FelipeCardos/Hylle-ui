7e1e66e636f329581b6a992ba51cc678675b8a16
FelipeCardos <felcarbib@gmail.com>
2025-03-22 09:38:34 +0000

    Start up ui

---
+++ hylle/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-3xl font-bold">NativeWind Funcionando? 🚀</Text>
+    </View>
+  );
+}
